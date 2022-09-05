f2d4316df259dc04c75fb60a95741145c8cbe9c0
Nakul <nagariya23nakul@gmail.com>
2022-09-05 19:17:36 +0530

    finishing handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -13,7 +13,13 @@ router.get('/', (req, res, next) => {
      * where to look for the file simply pass the name
      */
     // res.render(path.join(rootDir, "views", "shop.pug"))
-    res.render("shop", {prods: p, pageTitle: "Shop", hasProducts: p.length > 0})
+    res.render("shop", {
+        prods: p,
+        pageTitle: "Shop",
+        hasProducts: p.length > 0,
+        activeShop: true,
+        productCSS: true
+    })
 })
 
 module.exports = router;
