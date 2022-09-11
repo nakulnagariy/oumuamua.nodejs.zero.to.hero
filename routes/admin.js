10b4dd6158d39a9078cc5e4742072e89b2b17d72
Nakul <nagariya23nakul@gmail.com>
2022-09-11 23:14:00 +0530

    MVC pattern is done

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,12 @@
 const path = require('path');
 const express = require('express');
-const rootDir = require("../utils/path");
 const router = express.Router();
+const productController = require('../controllers/products')
 
-const products = [];
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-    res.render("add-product", {
-      pageTitle: "Add product",
-      productCSS: true,
-      path: "/admin/add-product",
-      formCSS: true,
-      activeAddProduct: true
-    })
-})
+router.get("/add-product", productController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-    products.push(({title: req.body.title}));
-    res.redirect('/');
-  });
-
-  console.log("git stash")
+router.post('/add-product', productController.postAddProduct);
   
-exports.routes = router;
-exports.products = products;
+module.exports = router;
