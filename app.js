4633af9a10d8478369896454dfec25124586066d
Nakul <nagariya23nakul@gmail.com>
2022-11-30 22:31:35 +0530

    sequelize completed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
-
+const { Sequelize } = require('sequelize');
 const app = express();
 
 // routes
@@ -13,41 +13,76 @@ const shopRoutes = require('./routes/shop');
 const sequelize = require('./utils/database');
 const errorController = require('./controllers/error');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use((req, res, next) => {
+    User.findByPk(1).then(user => {
+        req.user = user;
+        next();
+    }).catch(err => {
+        console.error(err);
+    })
+})
 // EJS
 app.set('view engine', 'ejs');
 app.set("views", "views");
-// Create the connection pool. The pool-specific settings are the defaults
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'jan2022@Nua361',
-//     database: 'complete_node'
-//   });
-
-// // For pool initialization, see above
-// pool.execute("SELECT * FROM product", function(err, rows, fields) {
-//     // Connection is automatically released when query resolves
-//     if(!err) {
-//         console.log("data", rows)
-//     }
-//  })
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404)
 
+// through means where this relationship exists, here CartItem will be another table
+
+Product.belongsTo(User, { constrain: true, onDelete: 'CASCADE' });
+
+User.hasMany(Product);
+
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 // sync will create the table from the model for you everytime you run the app if already not exists
-sequelize.sync().then(resulter => {
-    app.listen(8080, () => {
-        console.log("your app is started on port 8080");
+// force: true will override the existing table, dont do it in prod.
+
+/**
+ * below user is created for the dev purpose only
+ */
+sequelize
+    // .sync({ force: true })
+    .sync()
+    .then(result => {
+        return User.findByPk(1);
     })
-}).catch(err => {
-    console.log("error", err)
-})
+    .then(user => {
+        if (!user) {
+            return User.create({ name: "John", email: "john@example.com" })
+        }
+        return user;
+    })
+    .then(user => {
+        // console.log(user);
+        return user.createCart();
+      })
+    .then(cart => {
+        console.log("cart", cart);
+        app.listen(8080, () => {
+            console.log("your app is started on port 8080");
+        })
+    }).catch(err => {
+        console.log("error", err)
+    })
 
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,36 +1,40 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-    res.render("admin/edit-product", {
-        pageTitle: "Add product",
-        path: "/admin/add-product",
-        editing: false
-    })
+  res.render("admin/edit-product", {
+    pageTitle: "Add product",
+    path: "/admin/add-product",
+    editing: false
+  })
 }
 
 exports.postAddProduct = (req, res, next) => {
-    const { title, imageUrl, description, price } = req.body;
-    // create will create a modal and saves it, instead of build will not save
-    Product.create({
-      title: title,
-      imageUrl: imageUrl,
-      description: description,
-      price: price
-    }).then(result => {
-      console.log(result);
-      res.redirect('/admin/products');
-    }).catch((err) => {
-      console.error(err);
-    })
+  const { title, imageUrl, description, price } = req.body;
+  // create will create a modal and saves it, instead of build will not save
+  Product.create({
+    title: title,
+    imageUrl: imageUrl,
+    description: description,
+    price: price,
+    userId: req.user.id,
+  }).then(result => {
+    console.log(result);
+    res.redirect('/admin/products');
+  }).catch((err) => {
+    console.error(err);
+  })
 }
 
 exports.getEditProduct = (req, res, next) => {
-    const editMode = req.query.edit;
-    if (!editMode) {
-        return res.redirect("/");
-    }
-    const prodId = req.params.productId;
-    Product.findByPk(prodId).then(product => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.productId;
+  req.user
+    .getProducts({ where: { id: prodId } })
+    .then(products => {
+      const product = products[0];
       if (!product) {
         return res.redirect('/');
       }
@@ -46,32 +50,33 @@ exports.getEditProduct = (req, res, next) => {
 }
 
 exports.postEditProduct = (req, res, next) => {
-    const prodId = req.body.productId;
-    const updatedTitle = req.body.title;
-    const updatedPrice = req.body.price;
-    const updatedImageUrl = req.body.imageUrl;
-    const updatedDesc = req.body.description;
-    Product.findByPk(prodId).then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.imageUrl = updatedImageUrl;
-      product.description = updatedDesc;
-      return product.save();
-    }).then(result => {
-      console.log("result", result);
-      res.redirect('/admin/products');
-    }).catch(err => {
-      console.log(err)
-    })
-  };
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  Product.findByPk(prodId).then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl = updatedImageUrl;
+    product.description = updatedDesc;
+    return product.save();
+  }).then(result => {
+    console.log("result", result);
+    res.redirect('/admin/products');
+  }).catch(err => {
+    console.log(err)
+  })
+};
 
 exports.getProducts = (req, res, next) => {
-    Product.findAll().then(products => {
-        res.render("admin/products", {
-            prods: products,
-            pageTitle: "Admin products",
-            path: "/admin/products",
-        })
+  req.user.getProducts()
+    .then(products => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin products",
+        path: "/admin/products",
+      })
     }).catch(err => {
       console.log(err)
     })
@@ -79,13 +84,13 @@ exports.getProducts = (req, res, next) => {
 
 
 exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
-    Product.findByPk(prodId).then(product => {
-      product.destroy();
-    }).then(result => {
-      console.log("product deleted");
-    }).catch(err => {
-      console.log(err)
-    })
-    res.redirect('/admin/products');
-  };
+  const prodId = req.body.productId;
+  Product.findByPk(prodId).then(product => {
+    product.destroy();
+  }).then(result => {
+    console.log("product deleted");
+  }).catch(err => {
+    console.log(err)
+  })
+  res.redirect('/admin/products');
+};
